b7743b97bd3f70b1da099d153a79a0627c8692a4
lolynx <londevil@gmail.com>
2015-08-04 14:17:12 +0800

    fix equalStr

--- lorun/cext/diff.c
+++ lorun/cext/diff.c
@@ -20,11 +20,12 @@
 #include <sys/mman.h>
 
 int equalStr(const char *s, const char *s2) {
-    const char *c1 = s, *c2 = s2;
-    while (*c1 && *c2 && (*c1++ == *c2++))
-        ;
-    if ((*c1 | *c2) == 0)
-        return 1;
+    while (*s && *s2) {
+        if (*s++ != *s2++) {
+            return 1;
+        }
+    }
+
     return 0;
 }
 
